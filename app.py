a54531869ded1732ad2d81391614b8c0608ae627
kate_utkate <kate_utkate@vk.com>
2025-02-16 18:49:10 +0300

    added exception handling

--- app.py
+++ app.py
@@ -6,13 +6,21 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    data = None
-    if request.method == 'POST':
-        city_name = request.form['cityName']
-        state_code = request.form['stateCode']
-        country_code = request.form['countryCode']
+    try:
+        data = None
+        if request.method == 'POST':
+            city_name = request.form['cityName']
+            state_code = request.form['stateCode']
+            country_code = request.form['countryCode']
 
-        data = get_weather(city_name, state_code, country_code)
+            data = get_weather(city_name, state_code, country_code)
+    except ValueError as e:
+        data = {
+            'main': 'error: could not get weather data',
+            'description': None,
+            'icon': None,
+            'temperature': None,
+        }
 
     return render_template('index.html', data=data)
 
--- weather.py
+++ weather.py
@@ -19,13 +19,21 @@ def get_lat_lon(city_name, state_code, country_code, api_key):
     data = requests.get(
         f'http://api.openweathermap.org/geo/1.0/direct?q='
         f'{city_name},{state_code},{country_code}&appid={api_key}'
-    ).json()[0]
-    lat, lon = data.get('lat'), data.get('lon')
+    ).json()
+
+    if len(data) == 0:
+        return None, None
+
+    data_dict = data[0]
+    lat, lon = data_dict.get('lat'), data_dict.get('lon')
 
     return lat, lon
 
 
 def get_current_weather(lat, lon, api_key):
+    if lat is None or lon is None:
+        raise ValueError('Latitude and Longitude are required')
+
     data = requests.get(
         f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'
     ).json()
